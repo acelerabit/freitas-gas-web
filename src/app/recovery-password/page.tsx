e0492a9bb85ddd8912cd45540691dd0f42f118f4
gaba <gabrielferrsantos201@gmail.com>
2025-02-07 13:45:50 -0300

    fix: send email

--- src/app/recovery-password/page.tsx
+++ src/app/recovery-password/page.tsx
@@ -11,15 +11,49 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { fetchApi } from "@/services/fetchApi";
+import { useState } from "react";
+import { toast } from "sonner";
 
 export default function RecoveryPassword() {
+  const [email, setEmail]= useState("")
+
+
+  const onSubmit = async () => {
+    const response = await fetchApi(`/recovery-password`, {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+      }),
+    });
+
+    if (!response.ok) {
+      const respError = await response.json();
+      toast.error(respError.error, {
+        action: {
+          label: "Undo",
+          onClick: () => console.log("Undo"),
+        },
+      });
+      return;
+    }
+
+    toast.success("Código enviado como sucesso", {
+      action: {
+        label: "Undo",
+        onClick: () => console.log("Undo"),
+      },
+    });
+  };
+
+
   return (
     <main className="w-screen h-screen flex flex-col items-center justify-center">
       <Card className="w-full max-w-sm">
         <CardHeader>
-          <CardTitle className="text-2xl">Recovery password</CardTitle>
+          <CardTitle className="text-2xl">Recuperação de senha</CardTitle>
           <CardDescription>
-            Enter your email to receive a recovery code.
+            Entre com seu email para receber o código.
           </CardDescription>
         </CardHeader>
         <CardContent className="grid gap-4">
@@ -30,11 +64,12 @@ export default function RecoveryPassword() {
               type="email"
               placeholder="m@example.com"
               required
+              onChange={event => setEmail(event.target.value)}
             />
           </div>
         </CardContent>
         <CardFooter>
-          <Button className="w-full">Receive code</Button>
+          <Button type="button" onClick={onSubmit} className="w-full">Receber código</Button>
         </CardFooter>
       </Card>
     </main>
